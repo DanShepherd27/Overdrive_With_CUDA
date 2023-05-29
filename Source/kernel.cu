855857a5edfc98346b5a69a00fbb75e5784a3e68
DanShepherd27 <jdani20001027@gmail.com>
2023-05-29 18:00:49 +0200

    Optimize code, add timer

--- Source/kernel.cu
+++ Source/kernel.cu
@@ -1,36 +1,35 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "./kernel.cuh"
+#include "./PreciseTimer.h"
 #include <stdio.h>
 
-__global__ void applyGainKernel(float *samplesOfChannel, int gain, int arrayLength) {
+__global__ void applyGainAndNormalizeKernel(float *samplesOfChannel, int gain, float originalMagnitude, int arrayLength) {
     // Get thread ID.
     int threadID = blockDim.x * blockIdx.x + threadIdx.x;
     bool endReached = arrayLength - threadID <= 0;
 
-    if (threadID == 10000) {
+    /*if (threadID == 10000) {
         printf("ThreadID: %d \n", threadID);
         printf("Value before: %f \n", samplesOfChannel[threadID]);
-    }
+    }*/
     
     if (!endReached) {
+        // Apply gain
         samplesOfChannel[threadID] *= gain;
+
+        // Normalize value
+        if (samplesOfChannel[threadID] > originalMagnitude) {
+            samplesOfChannel[threadID] = originalMagnitude;
+        }
+        else if (samplesOfChannel[threadID] < -1 * originalMagnitude) {
+            samplesOfChannel[threadID] = -1 * originalMagnitude;
+        }
     }
 
-    if (threadID == 10000) {
+    /*if (threadID == 10000) {
         printf("Value after: %f \n", samplesOfChannel[threadID]);
-    }
-}
-
-__global__ void normalizeSamplesKernel(float *samplesOfChannel, float originalMagnitude, int arrayLength) {
-    int threadID = blockDim.x * blockIdx.x + threadIdx.x;
-    bool endReached = arrayLength - threadID <= 0;
-    if (!endReached && samplesOfChannel[threadID] > originalMagnitude) {
-        samplesOfChannel[threadID] = originalMagnitude;
-    }
-    if (!endReached && samplesOfChannel[threadID] < -1 * originalMagnitude) {
-        samplesOfChannel[threadID] = -1 * originalMagnitude;
-    }
+    }*/
 }
 
 
@@ -47,9 +46,11 @@ void kernel(const float *const *samples_by_channels, int numOfChannels, int arra
     
     // Array size per channel.
     int arraySize = arrayLength * sizeof(float);
-    const float* samplesOfLeftChannel = samples_by_channels[0];
-    printf("[BEFORE] Value of the 10.000th sample: %f \n", samplesOfLeftChannel[10000]);
-
+    /*const float* samplesOfLeftChannel = samples_by_channels[0];
+    printf("[BEFORE] Value of the 10.000th sample: %f \n", samplesOfLeftChannel[10000]);*/
+    printf("Kernel started...\n");
+    CPreciseTimer timer;
+    timer.StartTimer();
     for (size_t channelNumber = 0; channelNumber < numOfChannels; ++channelNumber)
     {
         // Initialize device pointers.
@@ -67,14 +68,15 @@ void kernel(const float *const *samples_by_channels, int numOfChannels, int arra
         dim3 gridSize(arrayLength % blockSize.x == 0 ? arrayLength/blockSize.x : arrayLength / blockSize.x + 1);
 
         // Launch CUDA kernel.    
-        applyGainKernel << < gridSize, blockSize >> > (dev_samplesOfChannel, gain, arrayLength);
-        normalizeSamplesKernel << <gridSize, blockSize >> > (dev_samplesOfChannel, originalMagnitude, arrayLength);
+        applyGainAndNormalizeKernel << < gridSize, blockSize >> > (dev_samplesOfChannel, gain, originalMagnitude, arrayLength);
         
         // Copy result back to host memory.
         cudaMemcpy((void*)samples_by_channels[channelNumber], dev_samplesOfChannel, arraySize, cudaMemcpyDeviceToHost);
     
     }
+    timer.StopTimer();
+    printf("Timer: %f ms.\n", timer.GetTimeMilliSec());
 
-    printf("CUDA has run. \n");
-    printf("[AFTER] Value of the 10.000th sample: %f \n", samplesOfLeftChannel[10000]);
+    /*printf("CUDA has run. \n");
+    printf("[AFTER] Value of the 10.000th sample: %f \n", samplesOfLeftChannel[10000]);*/
 }
--- Source/kernel.cuh
+++ Source/kernel.cuh
@@ -1,1 +1,3 @@
+#pragma once
+
 void kernel(const float *const *samples_by_channels, int numOfChannels, int arrayLength, int gain, float originalMagnitude);
